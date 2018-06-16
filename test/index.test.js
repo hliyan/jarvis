ee4907ee06fff52aef1c2ff096d55c1283f98a47
Hasitha Liyanage <hasitha.liyanage@trycake.com>
2018-06-16 18:05:41 +0530

    cleaned up test cases

--- test/index.test.js
+++ test/index.test.js
@@ -1,46 +1,54 @@
 const Jarvis = require('../index');
 
-const jarvis = new Jarvis();
-
-jarvis.addCommand({
-  command: 'simple',
-  handler: (context, data) => {
-    return 'tested: ' + data;
-  }
-});
-
-jarvis.addCommand({
-  command: 'repl',
-  handler: (context, data) => {
-    if (!context.activeCommand) {
-      context.startCommand('repl');
-      context.setCommandState({status: 'awaitInput'});
-      return 'Enter input: ';
+describe('basic command', async () => {
+  const jarvis = new Jarvis();
+  jarvis.addCommand({
+    command: 'simple',
+    handler: (context, data) => {
+      return 'tested: ' + data;
     }
+  });
 
-    if (context.state.status === 'awaitInput') {
-      const out = 'Handled: ' + data;
-      return out;
+  test('should return the expected output', async () => {
+    expect(await jarvis.send('simple')).toEqual('tested: simple');
+  });
+
+  test('should return null for unknown command', async () => {
+    expect(await jarvis.send('foo')).toBe(null);  
+  });
+
+});
+
+describe('interactive command', async () => {
+  const jarvis = new Jarvis();
+  jarvis.addCommand({
+    command: 'repl',
+    handler: (context, data) => {
+      if (!context.activeCommand) {
+        context.startCommand('repl');
+        context.setCommandState({status: 'awaitInput'});
+        return 'Enter input: ';
+      }
+  
+      if (context.state.status === 'awaitInput') {
+        const out = 'Handled: ' + data;
+        return out;
+      }
+  
     }
+  });
 
-  }
+  test('should enter the mode with the keyword', async () => {
+    expect(await jarvis.send('repl')).toEqual('Enter input: ');
+  });
+
+  test('should prompt for input in sequence', async () => {
+    expect(await jarvis.send('bar')).toEqual('Handled: bar');
+  });
+
+  test('should exit the mode with ..', async () => {
+    expect(await jarvis.send('..')).toEqual('Done with repl.');
+  });
 });
 
-test('basic command', async () => {
-  let out = null;
-  out = await jarvis.send('simple');
-  expect(out).toEqual('tested: simple');
-  out = await jarvis.send('foo');
-  expect(out).toBe(null);
-});
-
-test('interactive command', async () => {
-  let out = null;
-  out = await jarvis.send('repl');
-  expect(out).toEqual('Enter input: ');
-  out = await jarvis.send('bar');
-  expect(out).toEqual('Handled: bar');
-  out = await jarvis.send('..');
-  expect(out).toEqual('Done with repl.');
-});
 
